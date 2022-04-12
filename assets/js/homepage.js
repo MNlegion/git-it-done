d5c6882f6553b7c832259f4d52a54c7fbb76aad9
MNlegion <rkreuser30@gmail.com>
2022-04-12 14:46:00 -0500

    added additional js syntax and cleaned up index

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,30 @@
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  };
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {7
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please Enter a GitHub username");
+    }
+    console.log(event);
 };
 
-getUserRepos();
+userFormEl.addEventListener("submit", formSubmitHandler);
